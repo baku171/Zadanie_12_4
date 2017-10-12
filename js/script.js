61dee1cc879c4fc59ac5da6adf46396f4327d800
baku171 <pbak@esri.pl>
2017-10-12 09:28:59 +0200

    Zadanie 12.4

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var url = 'http://api.icndb.com/jokes/random';
+var button = document.getElementById('get-joke');
+button.addEventListener('click', function() {
+    getJoke();
+});
+
+var paragraph = document.getElementById('joke');
+
+function getJoke() {
+    var request = new XMLHttpRequest;
+    request.open('GET', url);
+    request.addEventListener('load',function() {
+        var response = JSON.parse(request.response);
+        paragraph.innerHTML = response.value.joke;
+    });
+    request.send();
+}
